085de17a1940666e68031cd2d9539cfc4739e7cc
Punnawich <Kurosougun@gmail.com>
2025-12-26 14:27:10 +0700

    test template

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
-@app.route('/hello/')
 def index():
-    return 'Hi my name is someone'
+    return render_template('index.html')
+
+
+
